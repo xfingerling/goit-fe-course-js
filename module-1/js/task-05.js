3523f90f2bcf415a5f96ab1cad77a8825feca611
Oleh Zavhorodnii <oleggenius95@gmail.com>
2019-11-28 18:52:07 +0200

    fixed task-5

--- module-1/js/task-05.js
+++ module-1/js/task-05.js
@@ -9,35 +9,28 @@ deliveryСountry = deliveryСountry.toLowerCase();
 switch (deliveryСountry) {
     case 'китай':
         costOfDelivery = 100;
-        message = `Доставка в ${deliveryСountry} будет стоить ${costOfDelivery} кредитов`;
-        alert(message);
         break;
 
     case 'чили':
         costOfDelivery = 250;
-        message = `Доставка в ${deliveryСountry} будет стоить ${costOfDelivery} кредитов`;
-        alert(message);
         break;
 
     case 'австралия':
         costOfDelivery = 170;
-        message = `Доставка в ${deliveryСountry} будет стоить ${costOfDelivery} кредитов`;
-        alert(message);
         break;
 
     case 'индия':
         costOfDelivery = 80;
-        message = `Доставка в ${deliveryСountry} будет стоить ${costOfDelivery} кредитов`;
-        alert(message);
         break;
 
     case 'ямайка':
         costOfDelivery = 120;
-        message = `Доставка в ${deliveryСountry} будет стоить ${costOfDelivery} кредитов`;
-        alert(message);
         break;
 
     default:
         message = 'В вашей стране доставка не доступна';
         alert(message);
 }
+
+message = `Доставка в ${deliveryСountry} будет стоить ${costOfDelivery} кредитов`;
+alert(message);
