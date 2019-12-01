186a8065cb9a49955346bd91632373ec6dca8057
Oleh Zavhorodnii <oleggenius95@gmail.com>
2019-12-01 19:17:05 +0200

    added for...of

--- module-2/js/task-3.js
+++ module-2/js/task-3.js
@@ -1,6 +1,12 @@
 'use strict';
 
 const findLongestWord = function(string) {
-    const sortStr = string.split(' ').sort((a, b) => b.length - a.length);
-    return sortStr.filter(el => el.length === sortStr[0].length);
+    const sortStrings = string.split(' ').sort((a, b) => b.length - a.length);
+    const maxLengthWords = sortStrings.filter(
+        el => el.length === sortStrings[0].length,
+    );
+
+    for (const word of maxLengthWords) {
+        console.log(word);
+    }
 };
