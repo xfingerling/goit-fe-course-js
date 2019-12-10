84debc5166b589f7ffffd00bfd6db4a81cfd9639
Oleh Zavhorodnii <oleggenius95@gmail.com>
2019-12-10 18:08:39 +0200

    fixed

--- module-2/js/task-3.js
+++ module-2/js/task-3.js
@@ -1,12 +1,25 @@
 'use strict';
 
-const findLongestWord = function(string) {
-    const sortStrings = string.split(' ').sort((a, b) => b.length - a.length);
-    const maxLengthWords = sortStrings.filter(
-        el => el.length === sortStrings[0].length,
-    );
+// const findLongestWord = function(string) {
+//     const sortStrings = string.split(' ').sort((a, b) => b.length - a.length);
+//     const maxLengthWords = sortStrings.filter(
+//         el => el.length === sortStrings[0].length,
+//     );
 
-    for (const word of maxLengthWords) {
-        console.log(word);
+//     for (const word of maxLengthWords) {
+//         console.log(word);
+//     }
+// };
+
+const findLongestWord = function(string) {
+    let arrStrings = string.split(' ');
+    let longestWord = arrStrings[0];
+
+    for (let i = 1; i < arrStrings.length; i++) {
+        if (arrStrings[i].length > longestWord.length) {
+            longestWord = arrStrings[i];
+        }
     }
+
+    return longestWord;
 };
--- module-2/js/task-7.js
+++ module-2/js/task-7.js
@@ -3,18 +3,18 @@
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
 const isLoginValid = function(login) {
-    return login.length >= 4 && login.length <= 16 ? true : false;
+    return login.length >= 4 && login.length <= 16;
 };
 
 const isLoginUnique = function(allLogins, login) {
-    return allLogins.includes(login) ? false : true;
+    return allLogins.includes(login);
 };
 
 const addLogin = function(allLogins, login) {
-    if (isLoginValid(login) === false) {
+    if (!isLoginValid(login)) {
         alert('Ошибка! Логин должен быть от 4 до 16 символов');
     } else {
-        if (isLoginUnique(allLogins, login) === false) {
+        if (!isLoginUnique(allLogins, login)) {
             alert('Такой логин уже используется!');
         } else {
             allLogins.push(login);
@@ -22,3 +22,8 @@ const addLogin = function(allLogins, login) {
         }
     }
 };
+
+addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
+addLogin(logins, 'robotGoogles'); // 'Такой логин уже используется!'
+addLogin(logins, 'Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin(logins, 'jqueryisextremelyfast'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
