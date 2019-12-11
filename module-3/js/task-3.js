4811cb5c2b0b8a109da594890fae15c6fe8e465f
Oleh Zavhorodnii <oleggenius95@gmail.com>
2019-12-11 17:57:42 +0200

    fix task 3

--- module-3/js/task-3.js
+++ module-3/js/task-3.js
@@ -1,12 +1,26 @@
 'use strict';
 
-function topSalary(salaries) {
-    let maxSalary = 0;
-    let stafName = null;
+// function topSalary(salaries) {
+//     let maxSalary = 0;
+//     let stafName = null;
 
-    for (const [name, salary] of Object.entries(salaries)) {
-        if (maxSalary < salary) {
-            maxSalary = salary;
+//     for (const [name, salary] of Object.entries(salaries)) {
+//         if (maxSalary < salary) {
+//             maxSalary = salary;
+//             stafName = name;
+//         }
+//     }
+
+//     return stafName;
+// }
+
+function findBestEmployee(salaries) {
+    let maxSalary = 0;
+    let stafName;
+
+    for (const name in salaries) {
+        if (maxSalary < salaries[name]) {
+            maxSalary = salaries[name];
             stafName = name;
         }
     }
