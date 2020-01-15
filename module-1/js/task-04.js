086a4c55e8cb6cdce14e55857c892ef2d8435708
Oleh Zavhorodnii <oleggenius95@gmail.com>
2020-01-15 17:25:43 +0200

    fixed variables

--- module-1/js/task-04.js
+++ module-1/js/task-04.js
@@ -2,22 +2,23 @@
 
 let credits = 23580;
 let pricePerDroid = 3000;
-let countDroids = prompt('Сколько дроидов вы хотите приобрести?', '');
-let message;
 
-if (countDroids === null || countDroids === '') {
-    message = 'Отменено пользователем!';
-    alert(message);
+const messageCancel = 'Отменено пользователем!';
+const messageWrong = 'Недостаточно средств на счету!';
+const messageSuccess = `Вы купили ${quantityDroids} дроидов, на счету осталось ${accountBalance} кредитов.`;
+
+const quantityDroids = prompt('Сколько дроидов вы хотите приобрести?', '');
+
+if (quantityDroids === null || quantityDroids === '') {
+    alert(messageCancel);
 } else {
-    countDroids = Number(countDroids);
-    let totalPrice = countDroids * pricePerDroid;
+    quantityDroids = Number(quantityDroids);
+    const totalPrice = quantityDroids * pricePerDroid;
 
     if (totalPrice > credits) {
-        message = 'Недостаточно средств на счету!';
-        alert(message);
+        alert(messageWrong);
     } else {
         let accountBalance = credits - totalPrice;
-        message = `Вы купили ${countDroids} дроидов, на счету осталось ${accountBalance} кредитов.`;
-        alert(message);
+        alert(messageSuccess);
     }
 }
