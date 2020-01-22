de8ee7588b454e44c1957db48734b0494b44ebe9
Oleh Zavhorodnii <oleggenius95@gmail.com>
2020-01-22 20:37:29 +0200

    fix task 3

--- module-1/js/task-03.js
+++ module-1/js/task-03.js
@@ -8,11 +8,10 @@ const userPass = prompt('Введите пароль', '');
 
 if (userPass === null) {
     message = 'Отменено пользователем!';
-    alert(message);
 } else if (userPass === ADMIN_PASSWORD) {
     message = 'Добро пожаловать!';
-    alert(message);
 } else {
     message = 'Доступ запрещен, неверный пароль!';
-    alert(message);
 }
+
+alert(message);
