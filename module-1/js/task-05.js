d5580eeca7ce6d06752e501b3bcb6b69a60e9e40
Oleh Zavhorodnii <oleggenius95@gmail.com>
2020-01-22 20:56:20 +0200

    fix task 5

--- module-1/js/task-05.js
+++ module-1/js/task-05.js
@@ -8,45 +8,41 @@ const costDeliveryJamaica = 120;
 
 let deliveryСountry = prompt('Укажите страну доставки', 'Украина');
 
-if (deliveryСountry !== null) {
-    deliveryСountry = deliveryСountry.toLowerCase();
-}
+if (deliveryСountry === null) {
+    alert('Отменено пользователем');
+} else {
+    switch (deliveryСountry.toLowerCase()) {
+        case 'китай':
+            alert(
+                `Доставка в ${deliveryСountry} будет стоить ${costDeliveryСhina} кредитов`,
+            );
+            break;
 
-switch (deliveryСountry) {
-    case 'китай':
-        alert(
-            `Доставка в ${deliveryСountry} будет стоить ${costDeliveryСhina} кредитов`,
-        );
-        break;
+        case 'чили':
+            alert(
+                `Доставка в ${deliveryСountry} будет стоить ${costDeliveryChile} кредитов`,
+            );
+            break;
 
-    case 'чили':
-        alert(
-            `Доставка в ${deliveryСountry} будет стоить ${costDeliveryChile} кредитов`,
-        );
-        break;
+        case 'австралия':
+            alert(
+                `Доставка в ${deliveryСountry} будет стоить ${costDeliveryAustralia} кредитов`,
+            );
+            break;
 
-    case 'австралия':
-        alert(
-            `Доставка в ${deliveryСountry} будет стоить ${costDeliveryAustralia} кредитов`,
-        );
-        break;
+        case 'индия':
+            alert(
+                `Доставка в ${deliveryСountry} будет стоить ${costDeliveryIndia} кредитов`,
+            );
+            break;
 
-    case 'индия':
-        alert(
-            `Доставка в ${deliveryСountry} будет стоить ${costDeliveryIndia} кредитов`,
-        );
-        break;
+        case 'ямайка':
+            alert(
+                `Доставка в ${deliveryСountry} будет стоить ${costDeliveryJamaica} кредитов`,
+            );
+            break;
 
-    case 'ямайка':
-        alert(
-            `Доставка в ${deliveryСountry} будет стоить ${costDeliveryJamaica} кредитов`,
-        );
-        break;
-
-    case null:
-        alert('Отменено пользователем');
-        break;
-
-    default:
-        alert('В вашей стране доставка не доступна');
+        default:
+            alert('В вашей стране доставка не доступна');
+    }
 }
