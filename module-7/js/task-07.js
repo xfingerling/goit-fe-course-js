51bfbcc1f5f7b85633795ff1989355fe4a14a670
Oleh Zavhorodnii <oleggenius95@gmail.com>
2020-02-13 22:31:54 +0200

    task 7 done

--- module-7/js/task-07.js
+++ module-7/js/task-07.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const input = document.getElementById('font-size-control');
-console.dir(text);
+
 function changeFontSize() {
     text.style.fontSize = input.value + 'px';
 }
---
+++ module-7/js/task-08.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const createBoxes = function(amount) {};
+
+console.dir(controls.children[0]);
