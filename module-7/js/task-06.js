68d52bb01f09ea18b73143c838c2dc0ead812922
Oleh Zavhorodnii <oleggenius95@gmail.com>
2020-02-13 21:51:12 +0200

    task 6 done

--- module-7/js/task-06.js
+++ module-7/js/task-06.js
@@ -2,4 +2,14 @@
 
 const input = document.getElementById('validation-input');
 
-input.addEventListener('focus');
+input.addEventListener('blur', function() {
+    let isValid = input.dataset.length == input.value.length ? true : false;
+
+    if (isValid) {
+        input.classList.add('valid');
+        input.classList.remove('invalid');
+    } else if (!isValid) {
+        input.classList.add('invalid');
+        input.classList.remove('valid');
+    }
+});
