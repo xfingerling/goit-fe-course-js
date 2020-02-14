f3ff58db35c2820442c904c368c2e79fb77d6a9a
Oleh Zavhorodnii <oleggenius95@gmail.com>
2020-02-14 20:35:40 +0200

    fix

--- module-7/js/task-01.js
+++ module-7/js/task-01.js
@@ -4,7 +4,7 @@ const listItem = document.querySelectorAll('.item');
 
 console.log(`В списке ${listItem.length} категории.`);
 
-Array.from(listItem).forEach(el => {
+listItem.forEach(el => {
     console.log(`Категория: ${el.firstElementChild.textContent}`);
     console.log(`Количество элементов: ${el.querySelectorAll('li').length}`);
 });
